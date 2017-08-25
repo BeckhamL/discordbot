155a381539e6a9a5c1c5385774399cc7d0e6400f
beckham <beckham.lam@mail.mcgill.ca>
2017-08-24 23:55:13 -0400

    added cheerio

--- commands/data/scrape.js
+++ commands/data/scrape.js
@@ -1,4 +1,14 @@
 const commando = require('discord.js-commando');
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var title;
+var score;
+var link;
+var rankScores = [];
+var posts = [];
+var largest = 0;
+var index = 0;
 
 class scrape extends commando.Command {
 
@@ -14,7 +24,34 @@ class scrape extends commando.Command {
     }
 
     async run(message, args) {
-        message.channel.send("You rolled a ");
+
+        request("https://www.reddit.com/r/2meirl4meirl/", function(error, response, body) {
+            if (error) {
+              console.log("Error: " + error);
+            }
+            console.log("Status code: " + response.statusCode);
+          
+            var $ = cheerio.load(body);
+          
+            $('div#siteTable > div.link').each(function(index) {
+              title = $(this).find('a.title').text().trim();
+              score = Number($(this).find('div.score.unvoted').attr('title'));
+              rankScores.push(score);
+              link = $(this).find('a.title').attr('href');
+          
+              posts.push('Title:' + title + 'Score:' + score + 'URL:' + link);
+          
+              fs.appendFileSync('reddit.txt', posts);
+            });
+          
+            for (var i = 0; i < rankScores.length; i++) {
+              if (rankScores[i] > largest) {
+                largest = rankScores[i];
+                index++;
+              }
+            }
+            console.log(posts[index]);
+          });
     }
 }
 
